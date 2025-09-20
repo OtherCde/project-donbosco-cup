9f6f455c5c69d4d186801487d089f0897d9feb3d
aleBsR <alejandrosandoval211@gmail.com>
2025-09-20 01:12:52 -0300

    solucione problemas con workflows

--- teams/admin.py
+++ teams/admin.py
@@ -75,7 +75,13 @@ class TeamAdmin(admin.ModelAdmin):
     Nota: Cada equipo representa una promoción específica
     """
 
-    list_display = ["name", "abbreviation", "tournament_category", "player_count", "upload_players_link"]
+    list_display = [
+        "name",
+        "abbreviation",
+        "tournament_category",
+        "player_count",
+        "upload_players_link",
+    ]
     list_filter = ["tournament_category__tournament", "tournament_category"]
     search_fields = ["name", "abbreviation"]
     ordering = ["tournament_category", "name"]
@@ -88,18 +94,24 @@ class TeamAdmin(admin.ModelAdmin):
         ),
         ("Visual", {"fields": ("logo_url",), "classes": ("collapse",)}),
     )
+
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
-            path('upload-players/', upload_players_from_excel, name='upload_players_from_excel'),
+            path(
+                "upload-players/",
+                upload_players_from_excel,
+                name="upload_players_from_excel",
+            ),
         ]
         return custom_urls + urls
-    
+
     def upload_players_link(self, obj):
         return format_html(
             '<a href="/admin/teams/team/upload-players/" class="button">Cargar desde Excel</a>'
         )
-    upload_players_link.short_description = 'Acciones'
+
+    upload_players_link.short_description = "Acciones"
 
     def player_count(self, obj):
         """Cuenta automáticamente el número de jugadores del equipo"""
